76f9f775fedc5e58cd488aee9c1daaa923414d05
npatel17Confed <nsp4898@gmail.com>
2020-07-31 05:33:36 -0400

    added validation for street number in javascript

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ function validateRegisterForm(e) {
     var pass = document.getElementById("pass").value;
     var pass2 = document.getElementById("pass2").value;
     var error = document.getElementById("error");
+    var stn = document.getElementById("stn").value;
 
     if (pass2 !== pass) {
         error.innerHTML = "The passwords must match.";
     } else if (pass.length < 8) {
         error.innerHTML = "Password must be at least 8 characters long.";
+    } else if(!stn.match(/^\d+/)) {
+        error.innerHTML = "Street number must be a number.";
     } else {
         window.location.href = "index.html";
     }
